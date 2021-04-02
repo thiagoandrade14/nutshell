d7668b8895ff8790b50300b517dc6406b052d107
haileebacon <51973557+haileebacon@users.noreply.github.com>
2021-04-02 19:25:39 -0400

    Update nutshell.cpp
    Still contains same functionality;
    Added layout from the powerpoint presentation as well as examples for easier reference

--- nutshell.cpp
+++ nutshell.cpp
@@ -1,26 +1,131 @@
 #include "nutshell.h"
 
-//Initializes the shell
-//For now, it simply gets and prints the username. 
+//                                                                                           Shell Init - Initializes the shell
+/*
+*   init all variables
+*   define (allocate storage) for some var/tables
+*   init all tables (e.g., alias table, command table)
+*   get PATH env variable (getenv())
+*   get HOME env variable (getenv())
+*   disable anything that may kill the shell
+*   any other tasks to be completed in init
+*/
 void shell_init() {
 	std::string username = getenv("USER");
 	std::cout << "Username is " << username << "." << std::endl;
 }
 
+
+//                                                                                          Get Command - Gets command line/parses
+/*
+*   init_scanner_and_parser();
+*   if(yyparse())
+*       understand_errors();
+*   else
+*       return (OK);
+*/
+std::string getCommand()
+{
+    std::string command;
+    getline(std::cin, command);
+    return command;
+}
+
+
+//                                                                                         Recover From Errors - Handling erroneous command line
+/*
+*   Find out if error occurred in middle of command,
+*   (if it has a "Tail")
+*   In this case you have to recover by "eating"
+*   the rest of the command.
+*   To do this: you may want to use yylex() directly, or
+*   handle clear things up in any other way.
+*/
+void recover_from_errors()
+{
+
+}
+
+
+//                                                                                         Proccess Command - Process your commands
+/*
+*   if(builtin)
+*       do_it();  run built-in commands - no fork
+*                 no exec; only your code + unix
+*                 system call.
+*   else
+*       execute_it(); execute general commands
+*                     using fork and exec        
+*/
+void processCommand()
+{
+
+}
+
+
+//                                                                                         Do it - Processes a built-in command
+/*
+*   switch(builtin)
+*       case ALIAS
+*       case CDHome
+*       case CDPath
+*       case UNALIAS
+*       case SETENV
+*       case PRINTENV
+*/
+void do_it()
+{
+
+}
+
+
+//                                                                                         Execute it - processing a command line
+/*
+*      Handle command execution, pipelining, i/o redirection, and background processing
+*      Utilize a command table whose components are plugged in during parsing by yacc
+*      Check command accessibility and executability
+*      if(!Executable())
+*      {
+*           Use access() system call
+*           nuterr("Command not Found")
+*           return;
+*      }
+*      Check io file existence in case of io-redirection
+*      if(check_in_file()==SYSERR)
+*      {
+*           nuterr("Can't read from : %s", srcf);
+*           return;
+*      }
+*      if(check_out_file()==SYSERR)
+*      {
+*           nuterr("Can't write to : %s", distf);
+*           return;
+*      }
+*      Build up the pipeline (create and set up pipe end points (using pipe, dup))
+*      Process background
+*/
+void execute_it()
+{
+
+}
+
+
+//                                                                                         Print Prompt - Prints cwd prompt
 //prints directory and asks for command input
-std::string printPrompt() {
+void printPrompt() {
 	std::string userInput;
 	char cwd[1024];
 	getcwd(cwd, 1024);
 	std::cout << cwd << ": ";
-	getline(std::cin, userInput);
-	return userInput;
 }
+
+
 
 int main() {
 	shell_init();
 	while (1) {
-		std::string userInput = printPrompt();
+        printPrompt();
+		std::string userInput = getCommand();
 		if (userInput == "bye") {
 			exit(0);
 		}
